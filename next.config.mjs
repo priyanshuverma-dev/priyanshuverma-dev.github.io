a1e21b53cd456c175c17660121d4c823a1fb3338
Priyanshu Verma <priyanshuverma@outlook.in>
2024-09-30 20:55:31 +0530

    deploy on vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
   eslint: {
     ignoreDuringBuilds: true,
   },
-
   images: {
-    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
