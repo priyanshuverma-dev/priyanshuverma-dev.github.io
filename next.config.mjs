3edc176d6440daeac84a69b093d8ff6b67e456d4
Priyanshu Verma <priyanshuverma@outlook.in>
2024-09-30 21:29:13 +0530

    debug 4

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,20 @@ const nextConfig = {
 
   images: {
     unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "dev-to-uploads.s3.amazonaws.com",
+        pathname: "/**",
+        port: "",
+      },
+      {
+        protocol: "https",
+        hostname: "utfs.io",
+        pathname: "/**",
+        port: "",
+      },
+    ],
   },
 };
 
