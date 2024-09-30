9dbaa2ecf161a1c9275954c0d138ca65fa95ad70
Priyanshu Verma <priyanshuverma@outlook.in>
2024-09-30 21:56:09 +0530

    image optimised

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  distDir: "out",
   eslint: {
     ignoreDuringBuilds: true,
   },
 
   images: {
-    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
