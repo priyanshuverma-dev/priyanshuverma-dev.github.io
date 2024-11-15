2e79ac790f14dcdb65602c324ac38eec73b6737e
Priyanshu Verma <priyanshuverma@outlook.in>
2024-11-15 09:18:29 +0000

    fixes: blogs caching

--- lib/utils.ts
+++ lib/utils.ts
@@ -2,18 +2,6 @@ import { Article, Post } from "./types";
 
 export function extractS3Url(url: string) {
   return url;
-  // Find the second "https" part in the URL
-  if (!url.includes("/https%3A%2F%2F")) return url;
-  const encodedS3Url = url.split("/https%3A%2F%2F")[1];
-
-  if (!encodedS3Url) {
-    return ""; // return null if no S3 URL is found
-  }
-
-  // Decode the percent-encoded S3 URL
-  const decodedS3Url = `https://${decodeURIComponent(encodedS3Url)}`;
-
-  return decodedS3Url;
 }
 
 export const fetchPost = async (slug: string): Promise<Post | null> => {
@@ -27,12 +15,7 @@ export const fetchPost = async (slug: string): Promise<Post | null> => {
 };
 export const fetchPosts = async (): Promise<Article[]> => {
   const res = await fetch(
-    "https://dev.to/api/articles?username=priyanshuverma",
-    {
-      next: {
-        revalidate: 3600,
-      },
-    }
+    "https://dev.to/api/articles?username=priyanshuverma?per_page=50",
   );
 
   const data = res.json();
